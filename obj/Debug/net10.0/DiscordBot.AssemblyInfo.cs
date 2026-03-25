82f6c393880e3c28f2e42bb5addf4613b021b20d
AVSPlayBackStudio <andrewplaybackstudio@gmail.com>
2026-03-25 22:59:33 +0000

    ApolloBot initial upload

--- obj/Debug/net10.0/DiscordBot.AssemblyInfo.cs
+++ obj/Debug/net10.0/DiscordBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb095349e239dc17afd276af03cce38a3694241")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
